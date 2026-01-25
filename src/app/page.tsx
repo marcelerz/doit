1505d253174e7cee761b98a340f0889b67b997e2
marcelerz <marcelerz@users.noreply.github.com>
2026-01-25 09:28:28 +0000

    deploy: fa6ea89e4e14781084d9d42330d01968dc671116

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { TodoApp } from "@/components/views/TodoApp";
-
-export default function Home() {
-  return <TodoApp />;
-}
--- src/components/settings/components/SettingsLoading.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export function SettingsLoading() {
-  return <div className="text-zinc-600 dark:text-zinc-400">Loading...</div>;
-}
